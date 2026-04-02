a247c00c1935d9251c14bc0e032eaffd4377ff95
zhoucheng <zhoucheng133@outlook.com>
2026-04-02 19:05:46 +0800

    添加对macOS的支持

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
 import sys
+
+from PyQt6.QtCore import QUrl
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
 
 os.environ["QT_QUICK_CONTROLS_STYLE"] = "Material"
 
+def get_qml_path(file_name):
+    if hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        # 开发环境路径
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+    return os.path.join(base_path, file_name)
+
 app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
-engine.load("main.qml")
+
+target_qml = get_qml_path("main.qml")
+engine.load(QUrl.fromLocalFile(target_qml))
 
 if not engine.rootObjects():
     sys.exit(-1)
